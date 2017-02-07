d7a93cfdca36a781471c7c2960205659b4e19413
(none) <(none)>
2017-02-07 14:35:34 -0700

    gulp init

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # ZeroPress
+
+I might be interested in using `gulp-uglify` for minification.
+
+If so:
+```bash
+$ npm install gulp-uglify --save dev
+```
+then in `gulpfile.js`
+```js
+// Other requires
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+
+gulp.task('useref', function () {
+  return gulp.src('src/*.html')
+  .pipe(useref())
+  // minify only if it's a JavaScript file
+  .pipe(gulpIf('*.js', uglify()))
+  .pipe(gulp.dest('dist'))
+});
+```
+
+I might also want to use `gulp-cssnano` to do the same thing for css
---
+++ gulpfile.js
@@ -0,0 +1,84 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync').create(),
+    requirejs = require('requirejs'),
+    del = require('del'),
+    gulpIf = require('gulp-if'),
+    imagemin = require('gulp-imagemin'),
+    runSequence = require('run-sequence');
+
+var config = {
+  baseUrl: 'src/js',
+  name: 'main',
+  out: 'dist/js/zeropress.js'
+};
+
+gulp.task('hello', function () {
+  console.log('Howdy');
+});
+
+gulp.task('sass', function () {
+  return gulp.src('src/scss/**/*.scss')
+  .pipe(sass()) // Using gulp-sass
+  .pipe(gulp.dest('src/css'))
+  .pipe(browserSync.reload({
+    stream: true
+  }))
+});
+
+gulp.task('watch', ['browserSync'], function () {
+  gulp.watch('src/scss/**/*.scss', ['sass']);
+  gulp.watch('src/*.html', browserSync.reload);
+  gulp.watch('src/js/**/*.js', browserSync.reload);
+
+  // Other watchers
+});
+
+gulp.task('browserSync', function () {
+  browserSync.init({
+    server: {
+      baseDir: 'src'
+    },
+  })
+});
+
+gulp.task('optimize', function () {
+  requirejs.optimize(config, function (buildResponse) {
+  //buildResponse is just a text output of the modules
+  //included. Load the built file for the contents
+  //Use config.out to get the optimezed file contents.
+  var contents = fs.readFileSync(config.out, 'utf8');
+  }, function (err) {
+  //optimization err callback
+  })
+});
+
+gulp.task('fonts', function () {
+  return gulp.src('src/fonts/**/*')
+  .pipe(gulp.dest('dist/fonts'))
+});
+
+gulp.task('clean:dist', function () {
+  return del.sync(['dist/**/*', '!dist/images', '!dist/images/**/*']);
+});
+
+gulp.task('images', function () {
+  return gulp.src('src/images/**/*.+(png|jpg|jpeg|gif|svg)')
+  // caching images that ran through imagemin
+    .pipe(cache(imagemin({
+      interlaced: true
+    })))
+  .pipe(gulp.dest('dist/images'))
+});
+
+gulp.task('cache:clear', function (callback) {
+  return cache.clearAll(callback)
+});
+
+gulp.task('build', function (callback) {
+  runSequence('clean:dist',
+    'sass',
+    ['images', 'fonts'],
+    callback)
+});
+
